f896c6b0c9f27fabf4831bba8d57662283811f3f
morgen-peschke <morgen.peschke@gmail.com>
2014-08-23 19:01:17 -0700

    Move config file to heredoc Work around Gnome keyboard layout stubbornness

--- keyboard/debian/install.sh
+++ keyboard/debian/install.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
 install -m 644 -o root -g root dvmcp.xkb         /usr/share/X11/xkb/symbols/dvmcp
-install -m 644 -o root -g root console-settings /etc/default/keyboard
+cat > /etc/default/keyboard <<EOF
+# KEYBOARD CONFIGURATION FILE
+# Consult the keyboard(5) manual page.
+
+XKBMODEL="pc105"
+XKBLAYOUT="dvmcp"
+XKBVARIANT=""
+XKBOPTIONS="terminate:ctrl_alt_bksp"
+
+BACKSPACE="guess"
+EOF
+chmod 644 /etc/default/keyboard
+
+cat > /etc/xdg/autostart/gnome-keyboard-settings-fix.desktop <<EOF
+[Desktop Entry]
+Type=Application
+TryExec=setxkbmap
+Exec=setxkbmap dvmcp
+Hidden=false
+NoDisplay=true
+X-GNOME-Autostart-enabled=true
+Name[en_US]=System Keyboard Fix
+Name=System Keyboard Fix
+Comment[en_US]=Apply system-wide keyboard settings to the session
+Comment=Applies system-wide keyboard settings to session
+EOF
+chmod 644 /etc/xdg/autostart/gnome-keyboard-settings-fix.desktop
+
 setxkbmap dvmcp
