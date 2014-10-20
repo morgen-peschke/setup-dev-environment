bdb522da84d5c4eb0be084e0441d49bc3bbf56e3
morgen-peschke <morgen.peschke@gmail.com>
2014-10-20 13:01:18 -0700

    Fix typo in command

--- emacs24/debian.sh
+++ emacs24/debian.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Ensure Dependencies 
+# Ensure Dependencies
 sudo apt-get install git-core autoconf automake
 
 sudo apt-get install libxaw7-dev libxpm-dev libpng12-dev libtiff5-dev libgif-dev libjpeg8-dev libgtk2.0-dev libncurses5-dev
 
-sudo apt-get build-deps emacs
+sudo apt-get build-dep emacs
 
 (
     # Grab the source from github
     sudo mkdir -p  /usr/local/src/emacs
     sudo chmod 777 /usr/local/src/emacs
-    
+
     git clone --depth 1 \
 	https://git.savannah.gnu.org/emacs.git \
 	/usr/local/src/emacs
-    
+
     cd /usr/local/src/emacs
-    
+
     # Configure
     ./autogen
     ./configure --prefix /usr/local/etc/emacs24
