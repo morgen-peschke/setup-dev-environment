78b976ebe838c585c7375722069ed600598c3107
morgen-peschke <morgen.peschke@gmail.com>
2015-05-30 14:11:12 -0700

    added install for keepass2

--- bash/debian.sh
+++ bash/debian.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Install The Silver Searcher (ag)
-which ag || (
-    # Ensure dependencies 
+which ag >/dev/null || (
+    # Ensure dependencies
     sudo apt-get install -y git-core automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
 
     # Get the source
     sudo mkdir -p  /usr/local/src/silversearcher
     sudo chmod 777 /usr/local/src/silversearcher
-    
+
     git clone --depth 1 \
 	https://github.com/ggreer/the_silver_searcher.git \
 	/usr/local/src/silversearcher
-	
+
     cd /usr/local/src/silversearcher/
-    ./build.sh && sudo make install    
+    ./build.sh && sudo make install
     cd /
     sudo rm -rf /usr/local/src/silversearcher
+)
+
+# Install Keepass2
+which keepass2 >/dev/null || (
+    sudo apt-get install keepass2
 )
