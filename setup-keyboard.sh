3593553b48175af257620cb2c7eb7c7bd1f0f1ac
morgen-peschke <morgen.peschke@gmail.com>
2015-07-29 12:45:32 -0700

    Adds keyboard setup for OSX (partially automated).

--- setup-keyboard.sh
+++ setup-keyboard.sh
@@ -7,4 +7,10 @@ if [ "$PLATFORM" = "debian" ]; then
         cd keyboard/debian
         sudo ./install.sh
     )
+elif [ "$PLATFORM" = "osx" ]; then
+    (
+        cd keyboard/osx
+        sudo cp -R "Morgen Custom Layout.bundle" "/Library/Keyboard Layouts/"
+        echo "You will probably need to restart to get the new keyboard layout"
+    )
 fi
