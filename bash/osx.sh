283b49b94cce26c8c1fc2017c594e55e59d67042
Morgen Peschke <morgen.peschke@gmail.com>
2016-01-26 09:14:25 -0800

    Install findutils on OSX

--- bash/osx.sh
+++ bash/osx.sh
@@ -2,4 +2,4 @@ sudo -k
 
 which brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install ag coreutils ispell
+brew install ag coreutils ispell findutils
