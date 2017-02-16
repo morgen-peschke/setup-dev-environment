14cfbb31b1cc01c295d722d1273387da9c13b4d9
Morgen Peschke <morgen.peschke@gmail.com>
2017-02-16 13:53:00 -0800

    Add Docker install on Debian

--- bash/debian.sh
+++ bash/debian.sh
@@ -27,3 +27,53 @@ which icdiff >/dev/null || (
     curl -s https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.7.3/icdiff | \
         sudo tee /usr/local/bin/icdiff > /dev/null && sudo chmod ugo+rx /usr/local/bin/icdiff
 )
+
+# Install docker
+which docker >/dev/null || (
+    # Steps from https://docs.docker.com/engine/installation/linux/ubuntu/
+
+    # Make sure we can use aufs
+    sudo apt-get install -y --no-install-recommends \
+         linux-image-extra-$(uname -r) \
+         linux-image-extra-virtual
+
+    # Setup the repository
+    sudo apt-get install -y --no-install-recommends \
+         apt-transport-https \
+         ca-certificates \
+         curl \
+         software-properties-common
+
+    FINGERPRINT='5811 8E89 F3A9 1289 7C07  0ADB F762 2157 2C52 609D'
+    # Install the Docker GPG key
+    lookup-key () { apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D; }
+    validate-key () { lookup-key | grep -F "$FINGERPRINT" >/dev/null; }
+
+    validate-key || {
+        curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+    }
+
+    # Verify Docker's GPG key
+    validate-key || {
+        echo "Unable to validate GPG key" >&2
+        lookup-key
+        exit 1
+    }
+
+    # Install Docker Repo
+    grep -F dockerproject /etc/apt/sources.list || {
+        sudo add-apt-repository \
+             "deb https://apt.dockerproject.org/repo/ \
+              ubuntu-$(lsb_release -cs) \
+              main"
+    }
+
+    sudo apt-get update
+    sudo apt-get -y install docker-engine
+
+    sudo groupadd docker || true
+    sudo usermod -aG docker $USER
+
+    echo "You'll need to logout and back in to use Docker without sudo
+    "
+)
