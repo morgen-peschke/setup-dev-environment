ea275bd66614eb4ffd4b55310a9230344caaf319
Morgen Peschke <morgen.peschke@gmail.com>
2017-02-20 11:56:41 -0800

    Remove unneeded nxhtml install

--- emacs24/install-settings.sh
+++ emacs24/install-settings.sh
@@ -3,9 +3,4 @@
     # Pull down settings
     mkdir -p ~/.emacs.d
     git clone --depth 1 https://github.com/morgen-peschke/emacs-config.git ~/.emacs.d
-
-    # Install nxhtml
-    mkdir -p ~/.emacs.d/lisp/nxhtml
-    git clone --depth 1 https://github.com/emacsmirror/nxhtml.git ~/.emacs.d/lisp/nxhtml
-
 )
