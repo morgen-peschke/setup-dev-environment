137729f0db909b6be5ea5667ab94534dfa956091
Morgen Peschke <morgen.peschke@gmail.com>
2019-11-01 13:10:08 -0700

    Replace which with hash

--- bash/debian.sh
+++ bash/debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install The Silver Searcher (ag)
-which ag >/dev/null || (
+hash ag >/dev/null || (
     # Ensure dependencies
     sudo apt-get install -y git-core automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
 
@@ -23,13 +23,13 @@ which ag >/dev/null || (
 sudo apt-get install -y keepassx keepass2 xclip
 
 # Install icdiff
-which icdiff >/dev/null || (
+hash icdiff >/dev/null || (
     curl -s https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.7.3/icdiff | \
         sudo tee /usr/local/bin/icdiff > /dev/null && sudo chmod ugo+rx /usr/local/bin/icdiff
 )
 
 # Install docker
-which docker >/dev/null || (
+hash docker >/dev/null || (
     # Steps from https://docs.docker.com/engine/installation/linux/ubuntu/
 
     # Make sure we can use aufs
--- bash/osx.sh
+++ bash/osx.sh
@@ -1,5 +1,5 @@
 sudo -k
 
-which brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+hash brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install ag coreutils ispell findutils icdiff rlwrap
