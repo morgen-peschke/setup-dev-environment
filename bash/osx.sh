ec1a59b7a236fafba59be61587a7ff67bebf251b
Morgen Peschke <morgen.peschke@gmail.com>
2022-08-01 10:44:01 -0700

    Add workaround for M1 homebrew install issue

--- bash/osx.sh
+++ bash/osx.sh
@@ -3,7 +3,11 @@ sudo -k
 
 hash brew || bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install ag coreutils ispell findutils icdiff rlwrap go jq
+if [ -f /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew install ag coreutils ispell findutils icdiff rlwrap go jq fx
 
 brew install --cask rectangle
 defaults write com.googlecode.iterm2 DisableWindowSizeSnap -integer 1
